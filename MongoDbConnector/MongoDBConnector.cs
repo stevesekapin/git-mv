ef481a237432c8fd643b4ac010b3970414687354
stevesekapin <138053027+stevesekapin@users.noreply.github.com>
2025-09-26 18:56:58 -0400

    Update MongoDBConnector.cs

--- MongoDbConnector/MongoDBConnector.cs
+++ MongoDbConnector/MongoDBConnector.cs
@@ -1,6 +1,32 @@
-﻿namespace MongoDbConnector;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
-public class Class1
+namespace MongoDbConnector;
+
+public class MongoDBConnector
 {
+    private readonly string _connectionString;
+    private readonly IMongoClient _client;
+    private readonly IMongoDatabase _adminDb;
 
+    public MongoDBConnector(string connectionString)
+    {
+        _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        _client = new MongoClient(_connectionString);
+        _adminDb = _client.GetDatabase("admin");
+    }
+
+    // No params; true if ping succeeds, false otherwise.
+    public bool Ping()
+    {
+        try
+        {
+            _adminDb.RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 }
