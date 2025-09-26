3b441f7a31ca91c83a75cde06ab8f1f741d33d4c
stevesekapin <138053027+stevesekapin@users.noreply.github.com>
2025-09-26 19:11:12 -0400

    Update MongoDBConnector.Test.cs

--- MongoDbConnector.Tests/MongoDBConnector.Test.cs
+++ MongoDbConnector.Tests/MongoDBConnector.Test.cs
@@ -1,10 +1,30 @@
-﻿namespace MongoDbConnector.Tests;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Testcontainers.MongoDb;
+using Xunit;
+using MongoDbConnector;
 
-public class UnitTest1
+public class MongoDBConnectorTests : IAsyncLifetime
 {
-    [Fact]
-    public void Test1()
-    {
+    // Spins up a real MongoDB in Docker for the tests
+    private readonly MongoDbContainer _mongo = new MongoDbBuilder()
+        .WithImage("mongo:7")
+        .Build();
 
+    public Task InitializeAsync() => _mongo.StartAsync();
+    public Task DisposeAsync()    => _mongo.DisposeAsync().AsTask();
+
+    [Fact]
+    public void Ping_ReturnsTrue_WhenContainerIsRunning()
+    {
+        var connector = new MongoDBConnector(_mongo.GetConnectionString());
+        connector.Ping().Should().BeTrue();
+    }
+
+    [Fact]
+    public void Ping_ReturnsFalse_WithInvalidConnection()
+    {
+        var bad = new MongoDBConnector("mongodb://127.0.0.1:1");
+        bad.Ping().Should().BeFalse();
     }
 }
